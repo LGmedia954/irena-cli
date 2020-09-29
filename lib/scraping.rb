97c65f5f393b252f08d3898958cca69b20a5e24b
LGmedia954 <LeslieGonzalez@me.com>
2020-09-28 22:41:35 -0400

    scraping

--- lib/scraping.rb
+++ lib/scraping.rb
@@ -6,45 +6,52 @@ require_relative './selections.rb'
 
 class Irena::Scraper #The About page is the most different one here
 
-topics = [Bioenergy, Geothermal, Hydropower, Ocean, Solar, Wind, About]
+topics = [get_bioenergy, get_geothermal, get_hydropower, get_ocean, get_solar, get_wind, get_about]
 
-    def Bioenergy
+    def get_bioenergy
+      subject = "Bioenergy"
       page = Nokogiri::HTML(open("https://www.irena.org/bioenergy"))
       title = page.css("h2.subsite-banner-text").text
       description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
     end
 
-    def Geothermal
+    def get_geothermal
+        subject = "Geothermal"
         page = Nokogiri::HTML(open("https://www.irena.org/geothermal"))
         title = page.css("h2.subsite-banner-text").text
         description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
     end
 
-      def Hydropower
+      def get_hydropower
+        subject = "Hydropower"
         page = Nokogiri::HTML(open("https://www.irena.org/hydropower"))
         title = page.css("h2.subsite-banner-text").text
         description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
       end
 
-      def Ocean
+      def get_ocean
+        subject = "Ocean"
         page = Nokogiri::HTML(open("https://www.irena.org/ocean"))
         title = page.css("h2.subsite-banner-text").text
         description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
       end
 
-      def Solar
+      def get_solar
+        subject = "Solar"
         page = Nokogiri::HTML(open("https://www.irena.org/solar"))
         title = page.css("h2.subsite-banner-text").text
         description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
       end
 
-      def Wind
+      def get_wind
+        subject = "Wind"
         page = Nokogiri::HTML(open("https://www.irena.org/wind"))
         title = page.css("h2.subsite-banner-text").text
         description = page.css("p.center-right-border").text.strip.gsub(/\s+/,' ')
       end
 
-      def About
+      def get_about
+        subject = "About"
         page = Nokogiri::HTML(open("https://www.irena.org/aboutirena"))
         title = page.css("h2.headline headline-md").text
         description = page.css("p.center-right-border")[0,1,2].text.strip.gsub(/\s+/,' ')
@@ -53,18 +60,34 @@ topics = [Bioenergy, Geothermal, Hydropower, Ocean, Solar, Wind, About]
 
       def find_topics
         self.topics.each do |t|
-        Renewables.find {|t| t.name == Renewables.name}
+        Renewables.find {|t| t.subject == Renewables.name}
         end
       end
 
-        def print_topics
+        def print_topics(*topics) #case input was not my first choice for code. I had a bit of difficulty here.
             self.find_topics
-            Renewables.detect do |item|
-              if topics.to_s.any? == Renewables.name && @energies[input.to_i-1]
+            topics
+            @energies[input.to_i-1].detect && 
+            case input
+              when @energies[input.to_i-1] == 0
+                get_bioenergy
+              when @energies[input.to_i-1] == 1
+                get_geothermal
+              when @energies[input.to_i-1] == 2
+                get_hydropower
+              when @energies[input.to_i-1] == 3
+                get_ocean
+              when @energies[input.to_i-1] == 4
+                get_solar
+              when @energies[input.to_i-1] == 5
+                get_wind
+              when @energies[input.to_i-1] == 6
+                get_about
+
                 puts "#{item.title}\n\n"
                 puts "#{item.description}"
                 puts "#{item.bullets}" || ""
-              enddz
+              
             end
           end
 
