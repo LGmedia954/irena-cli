c77d039cf7acde716c2ef6651599b0bbd9d96dff
LGmedia954 <LeslieGonzalez@me.com>
2021-09-28 08:32:47 -0400

    cleaned up gemfiles

--- lib/cli.rb
+++ lib/cli.rb
@@ -79,7 +79,7 @@ class CLI
 
     def process_input
       if ["1", "2", "3", "4", "5", "6"].include?(@input)
-        Irena::EnergyScraper.new.fetch_renewables(Irena::EnergyScraper.ENERGY_LINKS[@input][:url])
+        Irena::EnergyScraper.new.fetch_renewables(Irena::EnergyScraper.ENERGY_LINKS[@input][:url], Irena::EnergyScraper.ENERGY_LINKS[@input][:resource])
       end
     end
 
--- lib/irena_intl.rb
+++ lib/irena_intl.rb
@@ -1,7 +1,8 @@
 require 'pry'
-require 'colorize'
 require "open-uri"
 require "nokogiri"
+require 'colorize'
+require 'colorized_string'
 
 require_relative './cli.rb'
 require_relative './scraping.rb'
