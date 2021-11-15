5ae3298f6ce146fcd3f85b56c3a6415b51134903
LGmedia954 <LeslieGonzalez@me.com>
2021-11-15 17:31:46 -0500

    tested print vs puts

--- lib/cli.rb
+++ lib/cli.rb
@@ -45,7 +45,7 @@ class CLI     # Menu actions
             case input
             
                 when "1"
-                    p Irena::EnergyScraper::ENERGY_TYPES[0].colorize(:green)
+                    puts Irena::EnergyScraper::ENERGY_TYPES[0].colorize(:green)
 
                 when "2"
                     puts Irena::EnergyScraper::ENERGY_TYPES[1].colorize(:light_red) 
